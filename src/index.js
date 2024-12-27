5408cdb482ca8b994002921ec9108e55b83371cd
mangalam-mallah <mangalam8421@gmail.com>
2024-12-27 12:50:48 +0530

    utils and custom error written

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 // require('dotenv').config({path: './env'})
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERRR:", error);
+        throw error
+    })
+    
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is listening at port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => console.log("MongoDB connection failed!!", err))
